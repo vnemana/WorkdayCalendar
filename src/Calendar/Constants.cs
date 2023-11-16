12e4bc4364aa8afdc2b0a9c81e93ad5d9af1bc89
Adam Miller <adam@7thside.com>
2023-11-16 11:59:25 +0100

    General comments on code

--- src/Calendar/Constants.cs
+++ src/Calendar/Constants.cs
@@ -1,10 +1,14 @@
 namespace Calendar;
+// Nitpick: There are no constants in the constants.cs file
+
+// Idiomatic C#: Put these in separate files that are named by the type name. EndingWorkdayType.cs WorkdayEndTime.cs Direction.cs
 public enum EndingWorkdayType {
     CurrentDay,
     NextDay,
     PreviousDay
 };
 
+// I'd consider making some of these internal instead of public. If they are only used internal to this assembly for bookkeeping they don't need to be public if you arent asserting on them
 public record WorkdayEndTime {
     public EndingWorkdayType EndingWorkdayType {get; set;}
     public TimeOnly EndTime {get; set;}
--- src/Calendar/Workday.cs
+++ src/Calendar/Workday.cs
@@ -4,6 +4,10 @@ namespace Calendar;
 public class Workday {
     private TimeOnly _startTime;
     private TimeOnly _stopTime;
+    
+    // I'd separate the parsing from the object and make these an auto-property with an init only setter for construction time
+    // public TimeOnly StartTime { get; init; }
+    // public TimeOnly StopTime { get; init; }
     public bool SetStartTime(string hours) {
         return TimeOnly.TryParse(hours, out _startTime);
     }
@@ -11,21 +15,33 @@ public class Workday {
         return TimeOnly.TryParse(hours, out _stopTime);
     }
 
+    // Idiomatic C#: This should be a property and since it is the duration, I'd use the same "Total" in the name
+    // public double TotalMilliseconds => (_stopTime - _startTime).TotalMilliseconds; 
     public double GetMilliseconds() {
         return (_stopTime - _startTime).TotalMilliseconds;
     }
 
+    // Same comment about using Get in a method name, I'd name this more about what the method is doing: CalculateEndTime
     public WorkdayEndTime GetEndTime(DateTime inputDate, double remainingTime)
     {
+        // idiomatic C#: use var
         Direction direction = remainingTime > 0 ? Direction.Forward : Direction.Backward;
         var startTimeData = GetStartTime(inputDate, direction);
+        // idiomatic C#: use var
         DateTime endDateTime = startTimeData.Item1.AddMilliseconds(remainingTime * GetMilliseconds());
         return GetEndTimeForDate(inputDate, endDateTime, direction, startTimeData.Item2);
     }
-
+    
+    // This function and the end function has some duplicated complexity that I think can be simplified to improve readability.
+    // I think it might be a result of your approach, but if you look they both have this conditional on behaving one way or another based on direction
+    // Given your approach, I would probably split this and use the strategy pattern, one behavior for forward, and one behavior for backwards
+    
     private Tuple<DateTime, EndingWorkdayType> GetStartTime(DateTime inputDate, Direction direction) {
+        // idiomatic C#: use var
         TimeOnly inputStartTime = new TimeOnly(inputDate.Hour, inputDate.Minute, inputDate.Second);
+        // idiomatic C#: use var
         DateTime startDateTime = inputDate;
+        // idiomatic C#: use var
         EndingWorkdayType endingWorkdayType = EndingWorkdayType.CurrentDay;
         if (direction == Direction.Forward) {
             if (inputStartTime < _startTime) {
@@ -48,6 +64,7 @@ public class Workday {
         return new Tuple<DateTime, EndingWorkdayType> (startDateTime, endingWorkdayType);
     }
 
+    // Same comment about using Get in a method name
     private WorkdayEndTime GetEndTimeForDate(DateTime inputDate, DateTime endDateTime, Direction direction, EndingWorkdayType endingWorkdayType) {
         WorkdayEndTime result = new() {
             EndingWorkdayType = endingWorkdayType
